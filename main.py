f428a60704cd184cffb1f8cbcec835668dda0de2
nana <99609279+nana640@users.noreply.github.com>
2022-02-15 20:17:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from discord.ext import commands
 client = commands.Bot (command_prefix=".", self_bot=True, help_command=None)
 token = "insert_token_here"
 
+Output = "[Output] | "
+
 @client.event
 async def on_ready():
     print("Online!")
@@ -19,5 +21,47 @@ async def l(ctx):
     @client.command()
     async def swat(ctx, member):
         await ctx.send(f"{member} just commited fraud :^)")
+        
+        @client.command()
+        async def embed(ctx, title, *, description):
+            await ctx.message.delete()
+            embed=discord.Embed.(title=title, description=description)
+            await ctx.send(embed=embed)
+        
+@client.command()
+async def acsii(ctx, *, args):
+    await ctx.message.delete()
+    text = pyfiglet_format(args)
+    await ctx.send(f'```(text)```')
+    
+@client.command 
+async def request(ctx):
+    r = request.get('https://google.com')
+    print(r.status_code)
+    
+@client.command
+async def hypersquad(ctx, house):
+    await ctx.message.delete()
+    request = request.session()
+    headers = {
+        'Authorization': token,
+        'Content-Type': 'application/json'
+        
+    }
 
+
+   if house == "bravery":
+       payload = {'house_id': 1}
+       elif house == "brilliance":
+           payload = {'house_id': 2}
+           elif house == "balance":
+              pay load = {'house_id': 3}
+              
+              try:
+                  request.posT('https://discordapp.com/api/v6/hypersquad/online, headers=headers, json=payload')
+                  print(f"(Output)Successfully Set your hypersquad to (house)")
+                  except:
+                      print(f"(Output)Failed to set your hypersquad house.")
+           
+           
 client.run(token, bot=False)
